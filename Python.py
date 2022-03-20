c91356acd5a4f6e082d975bfcc62cffae794f2ce
Cenovaxs <tookenlocked@yahoo.co.id>
2022-03-20 19:20:54 +0700

    function

--- Python.py
+++ Python.py
@@ -1,12 +1,12 @@
 # Huruf (STRING)
 
-#type() mengecek type variabel
+# type() mengecek type variabel
 from multiprocessing import Condition
 
 
 message = 'string'
-print(type(message)) 
-#Id() mengecek lokasi dalam memory
+print(type(message))
+# Id() mengecek lokasi dalam memory
 print(id(message))
 
 # Quote
@@ -77,137 +77,140 @@ hasil = 9.87654321
 sentence = f'hasil = {hasil:.4}'
 print(sentence)
 # untuk mengecek method apa saja yang bisa digunakan
-#print(dir(message))
+# print(dir(message))
 # untuk mencari tau apa fungsi dari method tersebut
-#print(help(str))
+# print(help(str))
 
 # Angka (Integer dan float)
-num=10
+num = 10
 print(type(num))
-#floor division // ambil hasil bagi pertama contoh:
-print(7//2) #3
-print(4//2) #2
-#modulus % ambil sisa dari hasil bagi, biasanya digunakan untuk cek ganjil genap contoh:
-print(7%4) #3 
-#pangkat ** 
-print(2**4) #2pangkat4 = 16
-#*= kali jumlah, bisa juga -= , +=, /=, contoh
+# floor division // ambil hasil bagi pertama contoh:
+print(7//2)  # 3
+print(4//2)  # 2
+# modulus % ambil sisa dari hasil bagi, biasanya digunakan untuk cek ganjil genap contoh:
+print(7 % 4)  # 3
+# pangkat **
+print(2**4)  # 2pangkat4 = 16
+# *= kali jumlah, bisa juga -= , +=, /=, contoh
 num = 20
 num /= 4
-print(num) # hasilnya 20/4 = 5.0
-#abs absolut, menghilangkan minus
-print(abs(-30)) #hasilnya 30
-print(abs(30))  #hasilnya 30
-#round pembulatan
-print(round(3.49)) #hasilnya 3
-print(round(3.5)) #hasilnya 4
-print(round(3.634578, 4)) #dibulatkan sampai 4 angka dibelakang koma
-#comparison
-print(3==3) #apakah 3 sama dengan 3 
-print(3!=3) #apakah 3 tidak sama dengan 3?
-print(3>=2) #apakah 3 lebih besar sama dengan 2
-print(3<=2) #apakah 3 lebih kecil sama dengan 2
-#str(), int(), float() method untuk mengubah menjadi str, int ,float contoh
-str1='100'
+print(num)  # hasilnya 20/4 = 5.0
+# abs absolut, menghilangkan minus
+print(abs(-30))  # hasilnya 30
+print(abs(30))  # hasilnya 30
+# round pembulatan
+print(round(3.49))  # hasilnya 3
+print(round(3.5))  # hasilnya 4
+print(round(3.634578, 4))  # dibulatkan sampai 4 angka dibelakang koma
+# comparison
+print(3 == 3)  # apakah 3 sama dengan 3
+print(3 != 3)  # apakah 3 tidak sama dengan 3?
+print(3 >= 2)  # apakah 3 lebih besar sama dengan 2
+print(3 <= 2)  # apakah 3 lebih kecil sama dengan 2
+# str(), int(), float() method untuk mengubah menjadi str, int ,float contoh
+str1 = '100'
 print(float(str1))
 
 #List and Tuples
-matpel = ['sejarah','mat', 'fis','agama']
-print(len(matpel)) #melihat jumlah item dalam list
-print(matpel[0]) #melihat hanya isi item pertama
-print(matpel[-1]) #melihat hanya isi item terakhir
-print(matpel[0:2]) #melihat isi pertama sampai kedua
-print(matpel[:2]) #sama kyk diatas
-print(matpel[2:]) #melihat isi ketiga sampai akhir
-print(matpel[::-1]) #melihat isi reverse
-print(matpel[-1:0:-1]) #dari belakang ke nomor 2
+matpel = ['sejarah', 'mat', 'fis', 'agama']
+print(len(matpel))  # melihat jumlah item dalam list
+print(matpel[0])  # melihat hanya isi item pertama
+print(matpel[-1])  # melihat hanya isi item terakhir
+print(matpel[0:2])  # melihat isi pertama sampai kedua
+print(matpel[:2])  # sama kyk diatas
+print(matpel[2:])  # melihat isi ketiga sampai akhir
+print(matpel[::-1])  # melihat isi reverse
+print(matpel[-1:0:-1])  # dari belakang ke nomor 2
 
-#append tambah dibelakang
-matpel.append('seni') 
+# append tambah dibelakang
+matpel.append('seni')
 print(matpel)
-#tambah di posisi awal, bisa juga diatur posisi sesuai dengan keinginan
-matpel.insert(0,'seni')
+# tambah di posisi awal, bisa juga diatur posisi sesuai dengan keinginan
+matpel.insert(0, 'seni')
 
-matpel2=['seni', 'olahraga'] #apabila mau menggabungkan list tidak bisa menggunakan append
-matpel.append(matpel2) #karena akan menambahkan list di dalam list
+# apabila mau menggabungkan list tidak bisa menggunakan append
+matpel2 = ['seni', 'olahraga']
+matpel.append(matpel2)  # karena akan menambahkan list di dalam list
 print(matpel)
 
-#extend menggabungkan dua list
-matpel = ['sejarah','mat', 'fis','agama'] #gunakan extend untuk menggabungkan dua list
+# extend menggabungkan dua list
+# gunakan extend untuk menggabungkan dua list
+matpel = ['sejarah', 'mat', 'fis', 'agama']
 matpel.extend(matpel2)
 print(matpel)
 
-#membuang list
+# membuang list
 matpel.remove(matpel[0])
 print(matpel)
-#atau
+# atau
 matpel.remove('mat')
 print(matpel)
-#pop membuang yang paling belakang dan return value
+# pop membuang yang paling belakang dan return value
 popped = matpel.pop()
 popped2 = matpel.pop()
 print(matpel)
-print(popped) #olahraga
-print(popped2) #seni
+print(popped)  # olahraga
+print(popped2)  # seni
 
-#reverse sebutkan isi list dari belakang
-matpel = ['sejarah','mat', 'fis','agama', 'bahasa']
+# reverse sebutkan isi list dari belakang
+matpel = ['sejarah', 'mat', 'fis', 'agama', 'bahasa']
 matpel.reverse()
 print(matpel)
 
-#sort atur sesuai aplhabetical
+# sort atur sesuai aplhabetical
 matpel.sort()
 print(matpel)
-#bisa juga digunakan untuk mengurutkan angka dari kecil ke besar
-listangka = [6,24,52,100,2,5,20,80] 
+# bisa juga digunakan untuk mengurutkan angka dari kecil ke besar
+listangka = [6, 24, 52, 100, 2, 5, 20, 80]
 listangka.sort()
 print(listangka)
-#kalau mau terbalik bisa dengan:
+# kalau mau terbalik bisa dengan:
 listangka.sort(reverse=True)
 print(listangka)
 
-#sorted kyk sort tapi return value, ini kepakai kalau ga mau ganti originalnya
+# sorted kyk sort tapi return value, ini kepakai kalau ga mau ganti originalnya
 sorted_matpel = sorted(matpel)
 print(sorted_matpel)
 
-#min value minimum yang paling rendah
+# min value minimum yang paling rendah
 print(min(matpel))
-#max value paling tinggi
+# max value paling tinggi
 print(max(matpel))
-#sum jumlah
+# sum jumlah
 print(sum(listangka))
 
-#index cari urutan 
-print(matpel.index('fis')) #2 karena fis ada diurutan ke tiga
+# index cari urutan
+print(matpel.index('fis'))  # 2 karena fis ada diurutan ke tiga
 
-#cek apakah ada di dalam list
-print('fis' in matpel) #True
+# cek apakah ada di dalam list
+print('fis' in matpel)  # True
 
-#enumerate
+# enumerate
 for ea in enumerate(matpel):
-    print(ea) #hasil masih dalam bentuk list
-#yang rapih
+    print(ea)  # hasil masih dalam bentuk list
+# yang rapih
 for no, ea in enumerate(matpel):
     print(no, ea)
-#kalau mau mulai dari 1 bukan 0
+# kalau mau mulai dari 1 bukan 0
 for no, ea in enumerate(matpel, start=1):
     print(no, ea)
 
-#join jadi satu string
-matpel_join = ' - '.join(matpel) #bisa pake koma, atau apapun kalau mau yang lain tinggal ganti aja
+# join jadi satu string
+# bisa pake koma, atau apapun kalau mau yang lain tinggal ganti aja
+matpel_join = ' - '.join(matpel)
 print(matpel_join)
-#split ubah string jadi list
+# split ubah string jadi list
 matpel3 = matpel_join.split(" - ")
 print(matpel3)
 
-#tuple () kyk list tapi immutable gabs diganti
+# tuple () kyk list tapi immutable gabs diganti
 
-#set {} kyk list tapi gak ada order (unordered list) dan gak bisa doble ini cocok buat ngeeliminasi yang doble2
+# set {} kyk list tapi gak ada order (unordered list) dan gak bisa doble ini cocok buat ngeeliminasi yang doble2
 
-# list() convert menjadi list 
-atuple =(1,2,3,4,5)
+# list() convert menjadi list
+atuple = (1, 2, 3, 4, 5)
 alist = list(atuple)
-print(type(alist)) #jadi list
+print(type(alist))  # jadi list
 # set() convert menjadi set
 aset = set(atuple)
 print(type(aset))
@@ -215,72 +218,74 @@ print(type(aset))
 btuble = tuple(aset)
 print(type(btuble))
 
-#union ini khusus untuk set aja mengabungkan 2 set
+# union ini khusus untuk set aja mengabungkan 2 set
 seta = set(matpel)
 print(seta)
-setb = {'agama', 'sejarah','mat','geo','bio'}
+setb = {'agama', 'sejarah', 'mat', 'geo', 'bio'}
 print(setb)
 print(seta.union(matpel2))
 
-#intersection mengambil hanya yang sama dalam 2 set
+# intersection mengambil hanya yang sama dalam 2 set
 print(seta.intersection(setb))
 
-#difference mengambil hanya yang berbeda dalam 2 set
+# difference mengambil hanya yang berbeda dalam 2 set
 print(seta.difference(setb))
 
-#Dictionary kyk list tapi satu unitnya terdiri dari key dan value
+# Dictionary kyk list tapi satu unitnya terdiri dari key dan value
 print('\nDictionary\n')
-murid = {'name':'John', 'age': 25, 'courses':['Math','CompSci']}
-#mengambil dari dictionary
-print(murid['name']) #tapi ini tidak disarankan karena kalau ga ada maka bakal error
-#cara yang disarankan dalam mengambil dict:
-print(murid.get('name')) # kalau ini misalny ga ada maka akan none
+murid = {'name': 'John', 'age': 25, 'courses': ['Math', 'CompSci']}
+# mengambil dari dictionary
+# tapi ini tidak disarankan karena kalau ga ada maka bakal error
+print(murid['name'])
+# cara yang disarankan dalam mengambil dict:
+print(murid.get('name'))  # kalau ini misalny ga ada maka akan none
 print(murid.get('asfn o3wh1'))
-print(murid.get('asfn o3wh1','Not Found')) #kalau ga mau none bisa diganti dengan yang lain
-#cara mengupdate dict, kalau sudah ada key di dalamnya maka isinya akan diganti dengan yang baru
+# kalau ga mau none bisa diganti dengan yang lain
+print(murid.get('asfn o3wh1', 'Not Found'))
+# cara mengupdate dict, kalau sudah ada key di dalamnya maka isinya akan diganti dengan yang baru
 murid['phone'] = '089111555'
 print(murid)
-#atau .update cara lain mengupdate
+# atau .update cara lain mengupdate
 murid.update({'name': "Jane", 'phone': '750384', 'school': 'OL'})
 print(murid)
 #del hapus
 del murid['school']
 print(murid)
-#pop hapus dan return value
+# pop hapus dan return value
 phone = murid.pop('phone')
 print(murid)
 print(phone)
-#len untuk liat berapa key
+# len untuk liat berapa key
 print(len(murid))
-#key untuk kasih liat key
+# key untuk kasih liat key
 print(murid.keys())
-#values untuk kasih liat values
+# values untuk kasih liat values
 print(murid.values())
-#items untuk kasih liat semuanya
+# items untuk kasih liat semuanya
 print(murid.items())
-#dictionary loop
+# dictionary loop
 for key in murid:
-    print(key) #metode ini cuma akan menampilkan key
-#kalau mau menampilkan key dan values maka:
+    print(key)  # metode ini cuma akan menampilkan key
+# kalau mau menampilkan key dan values maka:
 for key, value in murid.items():
-    print(key,value)
-#if kalau True maka lanjut kalau False maka stop
+    print(key, value)
+# if kalau True maka lanjut kalau False maka stop
 print('\nConditionals and Booleans\n')
 if True:
     print('if')
-#is membandingkan apakah berada dalam satu memori yang sama
-a=[1,2,3]
-b=[1,2,3]
+# is membandingkan apakah berada dalam satu memori yang sama
+a = [1, 2, 3]
+b = [1, 2, 3]
 print(a == b)
 print(a is b)
 print(id(a))
-print(id(b)) # a is not b karena 
-#else
+print(id(b))  # a is not b karena
+# else
 if False:
     print('if')
 else:
     print('else')
-#elif
+# elif
 if False:
     print('if')
 elif True:
@@ -288,7 +293,7 @@ elif True:
 else:
     print('else')
 
-#and or
+# and or
 user = 'Admin'
 logged_in = True
 
@@ -296,20 +301,139 @@ if user == 'Admin' and logged_in:
     print('Welcome Admin')
 else:
     print('Bad Creds')
-#not 
+# not
 if not logged_in:
     print('please login')
 else:
     print("login successful")
 
-#apa saja yang termasuk false:
-#False
-#None
-#0
-#empty sequence contohnya "", '', [], {}, ()
+# apa saja yang termasuk false:
+# False
+# None
+# 0
+# empty sequence contohnya "", '', [], {}, ()
 
 condition = -10
 if condition:
     print('true')
 else:
     print('false')
+
+# loop
+print('Loop')
+# break
+
+nums = [1, 2, 3, 4, 5]
+
+for num in nums:
+    if num == 3:
+        print('found')
+        break
+    print(num)  # loop akan berhenti dan num = 3 tidak diprint
+
+# continue
+
+for num in nums:
+    if num == 3:
+        print('found')
+        continue
+    print(num)  # angka 3 tidak di print dan loop tetap sampai akhir
+
+#loop in loop
+for num in nums:
+    for letter in 'abc':
+        # ini akan menghasilkan 1a, 1b, 1c, 2a, 2b, dst sampai 5c
+        print(num, letter)
+
+# range loop sebanyak x kali
+for i in range(10):
+    print(i)  # ini akan ngeloop sampai 10 kali dari 0-9
+
+# while kalau pakai while bisa infinite loop
+x = 0
+while x < 10:
+    print(x)
+    x += 1  # ini akan ngeloop sampai 10 kali dari 0-9
+
+# atau
+
+x = 0
+while True:
+    if x == 10:
+        break
+    print(x)
+    x += 1
+
+# function
+
+
+def hello_func():
+    pass  # pass ini menghindari error akibat function yang kosong
+
+
+print(hello_func)  # ini hanya memperlihatkan nama func dan lokasi di memory
+print(hello_func())  # ini akan menjalankan funtion
+
+
+def hello_func():
+    print("Hello")
+
+
+hello_func()
+
+# Return
+# kalau di excecute function akan sama dengan value return
+
+
+def hello_func():
+    return 'return success!'
+
+
+print(hello_func())
+# upper bisa di chain bisa dilakukan karena return dari hello func adalah str
+print(hello_func().upper())
+
+# argument variabel
+
+
+def hello_func(arg):
+    return f'{arg} Function berhasil'
+
+
+print(hello_func('test'))
+
+# multiple argumen variabel
+
+
+def hello_func(arg, nama='kamu'):  # nama default value
+    return f'{arg} {nama} Function berhasil'
+
+
+print(hello_func('hallo'))
+print(hello_func('halo', nama='saya'))  # default value bisa diganti
+
+# args kwargs
+
+
+def student_info(*args, **kwargs):
+    print(args)  # args dalam hal ini adalah tuple
+    print(kwargs)  # kwargs dalam hal ini adalah dictionary
+
+
+student_info('math', 'art', age=20, name='john')
+
+# versi yang lebih rapih
+print('versi yang lebih rapih')
+
+
+def student_info(*args, **kwargs):
+    print(args)  # args dalam hal ini adalah tuple
+    print(kwargs)  # kwargs dalam hal ini adalah dictionary
+
+
+courses = ['math', 'art']
+info = {'name': 'john', 'age': 20}
+
+student_info(info, courses)
+# ini gak bisa info duluan harus args dulu baru kwargs
+student_info(*courses, **info)
